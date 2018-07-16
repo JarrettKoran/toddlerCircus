9fed259865ac8c1b26d3bf0cbcaf9f032d75cd9d
JarrettKoran <jarrettkoran@gmail.com>
2018-07-16 11:56:20 -0700

    Added noises

--- circusGame/Assets/Scripts/popScript.cs
+++ circusGame/Assets/Scripts/popScript.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class popScript : MonoBehaviour {
 
+	public AudioClip popSound;
+
+	private AudioSource source;
+	private float volLow = .4f;
+	private float volHigh = .9f;
+	private float pitchLow = .65f;
+	private float pitchHigh = 1.5f;
+
+	void Awake ()
+	{
+		source = GetComponent<AudioSource>();
+		popSound = source.clip;
+		source.pitch = Random.Range(pitchLow, pitchHigh);
+		source.PlayOneShot(popSound, Random.Range(volLow, volHigh));
+	}
+
 	// Use this for initialization
 	void Start () {
 		Destroy(this.gameObject,1.0f);
