dfa7bd2261a4f62e8870ebf780142e07072fd18a
kevinmckinnis928 <kevinmckinnis928@gmail.com>
2018-08-08 12:16:20 -0700

    Data Stuff
    It SHOULDN'T break

---
+++ circusGame/Assets/Scripts/QuitFromMainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitFromMainMenu : MonoBehaviour {
+
+    public void QuitGame()
+    {
+        PlayerPrefs.DeleteAll();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+}
---
+++ circusGame/Assets/Scripts/SubjectName.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SubjectName : MonoBehaviour {
+
+    public InputField name;
+
+    public void ButtonPushed()
+    {
+        if(name.text != "")
+        {
+            PlayerPrefs.SetString("subjectName", name.text);
+        }
+        Debug.Log(name.text);
+        Debug.Log(PlayerPrefs.GetString("subjectName"));
+    }
+}
